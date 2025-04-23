84551694e86848c27a8732cffdcacadb5e4d0db6
AndreaPotenza <andreapoten3@gmail.com>
2025-04-23 11:52:06 +0200

    add metodi televisori

--- src/org/lesson/java/inheritance/Main.java
+++ src/org/lesson/java/inheritance/Main.java
@@ -7,12 +7,20 @@ public static void main(String[] args) {
     
     Prodotto televisione = new Prodotto("s43", "samsung", new BigDecimal(500),new BigDecimal(23));
 
-    System.out.println(televisione.getCodice());
     televisione.setNome("carlo");
+    System.out.println(televisione.getCodice());
     System.out.println(televisione.getNome());
     System.out.println(televisione.getMarca());
     System.out.println(televisione.getPrezzo());
     System.out.println(televisione.getIva());
+
+    Televisore samsung = new Televisore("LG42A", "LG", new BigDecimal(800), new BigDecimal(23), 38, true);
+
+    samsung.setDimensione(43);
+    samsung.setIsSmart(false);
+    System.out.println(samsung.getNome());
+    System.out.println(samsung.getIsSmart());
+    System.out.println(samsung.getDimensione());
 
 }
 }
--- src/org/lesson/java/inheritance/Televisore.java
+++ src/org/lesson/java/inheritance/Televisore.java
@@ -4,8 +4,30 @@ import java.math.BigDecimal;
 
 public class Televisore extends Prodotto{
 
-    public Televisore(String nome, String marca, BigDecimal prezzo, BigDecimal iva, int dimensioni, Boolean smart){
+    protected int dimensioni;
+    protected Boolean isSmart; 
+
+    public Televisore(String nome, String marca, BigDecimal prezzo, BigDecimal iva, int dimensioni, Boolean isSmart){
         super(nome, marca, prezzo, iva);
+
+        this.dimensioni = dimensioni;
+    }
+
+    public int getDimensione() {
+
+        return this.dimensioni;
+    }
+
+    public void setDimensione(int dimensioni) {
+        this.dimensioni = dimensioni;
+    }
+
+    public Boolean getIsSmart() {
+        return this.isSmart;
+    }
+
+    public void setIsSmart(Boolean isSmart) {
+        this.isSmart = isSmart;
     }
 
 }
